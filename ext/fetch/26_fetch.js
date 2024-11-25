633553a6f180b2a38bb25e1cdd42b131c8d1f607
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-11-25 10:07:00 -0800

    ext/fetch: drop auth headers, cookies on redirect to a different origin
    Backport of denoland/deno@7456255c (CVE-2025-21620): authorization, proxy-authorization and cookie are removed from the request before following a redirect to a different host (other than a subdomain) or from https to http.
    
    Co-authored-by: Espen Hovlandsdal <espen@hovlandsdal.com>
    Co-authored-by: Bartek Iwańczuk <biwanczuk@gmail.com>
    (cherry picked from commit 7456255cd10286d71363fc024e51b2662790448a)

--- ext/fetch/26_fetch.js
+++ ext/fetch/26_fetch.js
@@ -29,6 +29,7 @@ const {
   PromisePrototypeCatch,
   SafeArrayIterator,
   String,
+  StringPrototypeEndsWith,
   StringPrototypeStartsWith,
   StringPrototypeToLowerCase,
   TypeError,
@@ -61,6 +62,12 @@ const REQUEST_BODY_HEADER_NAMES = [
   "content-language",
   "content-location",
   "content-type",
+];
+
+const REDIRECT_SENSITIVE_HEADER_NAMES = [
+  "authorization",
+  "proxy-authorization",
+  "cookie",
 ];
 
 /**
@@ -250,12 +257,14 @@ function httpRedirectFetch(request, response, terminator) {
   if (locationHeaders.length === 0) {
     return response;
   }
+
+  const currentURL = new URL(request.currentUrl());
   const locationURL = new URL(
     locationHeaders[0][1],
     response.url() ?? undefined,
   );
   if (locationURL.hash === "") {
-    locationURL.hash = request.currentUrl().hash;
+    locationURL.hash = currentURL.hash;
   }
   if (locationURL.protocol !== "https:" && locationURL.protocol !== "http:") {
     return networkError("Can not redirect to a non HTTP(s) url");
@@ -294,6 +303,28 @@ function httpRedirectFetch(request, response, terminator) {
       }
     }
   }
+
+  // Drop confidential headers when redirecting to a less secure protocol
+  // or to a different domain that is not a superdomain
+  if (
+    locationURL.protocol !== currentURL.protocol &&
+      locationURL.protocol !== "https:" ||
+    locationURL.host !== currentURL.host &&
+      !isSubdomain(locationURL.host, currentURL.host)
+  ) {
+    for (let i = 0; i < request.headerList.length; i++) {
+      if (
+        ArrayPrototypeIncludes(
+          REDIRECT_SENSITIVE_HEADER_NAMES,
+          byteLowerCase(request.headerList[i][0]),
+        )
+      ) {
+        ArrayPrototypeSplice(request.headerList, i, 1);
+        i--;
+      }
+    }
+  }
+
   if (request.body !== null) {
     const res = extractBody(request.body.source);
     request.body = res.body;
@@ -408,6 +439,19 @@ function abortFetch(request, responseObject, error) {
     if (response.body !== null) response.body.error(error);
   }
   return error;
+}
+
+/**
+ * Checks if the given string is a subdomain of the given domain.
+ *
+ * @param {String} subdomain
+ * @param {String} domain
+ * @returns {Boolean}
+ */
+function isSubdomain(subdomain, domain) {
+  const dot = subdomain.length - domain.length - 1;
+  return dot > 0 && subdomain[dot] === "." &&
+    StringPrototypeEndsWith(subdomain, domain);
 }
 
 /**
